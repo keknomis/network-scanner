d30d2f36e6a6257910955396090cb28252d48a21
_skm <7682435-_skm@users.noreply.gitlab.com>
2021-03-27 17:09:34 +0100

    cleanup

---
+++ scanner/threadedScanner.py
@@ -0,0 +1,85 @@
+import sys
+from socket import socket, gethostname, gethostbyname
+import threading
+
+class PortScanner():
+    """
+    PortScanner class
+    init with host, startPort, endPort, threads
+    host -> host address to scan
+    startPort -> port to start scanning at
+    endPort -> port to stop scanning at
+    threads -> number of threads for port scanning
+    """
+    def __init__(self, **args):
+        if "host" in args:
+            self.host = host
+        else:
+            self.host = gethostbyname(gethostname())
+
+        if "startPort" in args:
+            self.startPort = args["startPort"]
+        else:
+            self.startPort = 1
+
+        if "endPort" in args:
+            self.endPort = args["endPort"]
+        else:
+            self.endPort = 65535
+
+        if "threads" in args:
+            self.threads = args["threads"]
+        else:
+            self.threads = 32
+
+        self.openPorts = []
+
+    def portIsOpen(self, host, port):
+        """
+        return True if {port} on {host} is open, otherwise return False
+        """
+        Socket = socket()
+        try:
+            Socket.connect((host, port))
+        except ConnectionRefusedError:
+            return False
+        except:
+            print("ERROR")
+            return False
+        return True
+
+    # helper function for multithreading
+    def portScan(self, host, ports):
+        while ports:
+            port = ports.pop(0)
+            if self.portIsOpen(host, port):
+                self.openPorts.append(port)
+
+    # scans ports in range, uses multithreading
+    def portScanner(self, host, startPort, endPort, threads):
+        ports = list(range(startPort, endPort+1))
+        threads = [threading.Thread(target=self.portScan, args = (host, ports)) for _ in range(threads)]
+        for thread in threads:
+            thread.start()
+        for thread in threads:
+            thread.join()
+
+    # def scan(self, host=self.host, startPort=self.startPort, endPort=self.endPort, threads=self.threads):
+    def scan(self, host=None, startPort=None, endPort=None, threads=None):
+        """
+        scan ports in range {startPort}, {endPort} on host
+        default {host} is local machine
+        default {startPort} is 1
+        default {endPort} is 65535
+        """
+        if host is None:
+            host = self.host
+        if startPort is None:
+            startPort = self.startPort
+        if endPort is None:
+            endPort = self.endPort
+        if threads is None:
+            threads = self.threads
+        self.openPorts = []
+        self.portScanner(host, startPort, endPort, threads)
+        return self.openPorts
