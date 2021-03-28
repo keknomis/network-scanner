5097c9998423d9fc4ed5399d33717be7aed2120a
_skm <7682435-_skm@users.noreply.gitlab.com>
2021-03-28 04:45:56 +0200

    added portAnalyzer.py

---
+++ portAnalyzer.py
@@ -0,0 +1,19 @@
+knownPorts = {
+21 : "FTP",
+22 : "SSH",
+23 : "telnet",
+25 : "SMTP",
+53 : "DNS",
+443 : "http, https",
+110 : "POP3",
+135 : "Windows RPC",
+136 : "Windows NetBIOS",
+137 : "Windows NetBIOS",
+138 : "Windows NetBios",
+1433 : "Windows SQL Server",
+1434 : "Windows SQL Server",
+}
+
+def portAnalyzer(port):
+    return knownPorts.get(port, "")
+
